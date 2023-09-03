247adbac6e87c022bb351fbc75f74306c6d35caa
Adam Ure <adamqure@icloud.com>
2023-09-03 14:16:43 -0600

    Add note for Week 3 Ansible Assignment

--- helloworld.js
+++ helloworld.js
@@ -8,7 +8,7 @@ http.createServer(function (request, response) {
    response.writeHead(200, {'Content-Type': 'text/plain'})
 
    // Send the response body as "Hello World"
-   response.end('Week 2 Homework Successful!\n')
+   response.end('Week 2 Homework Successful!\n\nUpdated on September 3, 2023 for Week 3 Ansible Playbook\n')
 }).listen(3333)
 
 // Console will print the message
